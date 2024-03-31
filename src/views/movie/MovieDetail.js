7ea33f35af064b26a3e2de1c94e9a313b9eced91
Dayun Lee <wohd7877@naver.com>
2024-03-31 23:40:09 +0900

    [feat] 영화 설명 화면 설계

--- src/views/movie/MovieDetail.js
+++ src/views/movie/MovieDetail.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './MovieDetail.css'
 import bookmark from './bookmark.png'
 import movieImg from './Rectangle.png';
+import star from './star.png';
+import starN from './star_unclick.png';
+import back from './back.png';
 import {Link, useNavigate} from "react-router-dom";
 
 const MovieDetail=() =>{
     return (
-        <div>
+        <div className="mobile">
+            <div className="back_img">
+                <img src={back} width={30} height={30}/>
+            </div>
             <div className="bookmarkMovie">
                 <img src={bookmark} width={50} height={50}/>
             </div>
@@ -16,6 +22,33 @@ const MovieDetail=() =>{
             <div className="title">
                 영화명
             </div>
+            <div className="starGroup">
+                <img className="star_image" src={star} width={30} height={30}/>
+                <img className="star_image" src={star} width={30} height={30}/>
+                <img className="star_image" src={star} width={30} height={30}/>
+                <img className="star_image" src={star} width={30} height={30}/>
+                <img className="star_image" src={star} width={30} height={30}/>
+                <div className="star_text">
+                    (5)
+                </div>
+            </div>
+            <div className="movie_text">
+                영화설명~~~~~~~~~~~~~~~~~~~~~<br/>
+                영화설명~~~~~~~~~~
+            </div>
+            <div className="category">
+                <div className="category_box">
+                    <div className="category_text">
+                        #스릴러
+                    </div>
+                </div>
+                <div className="category_box">
+                    <div className="category_text">
+                        #로맨스
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 }
