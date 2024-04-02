428b2956053d5e2c8400f6f0cdd058bc504badb6
Dayun Lee <wohd7877@naver.com>
2024-04-02 11:53:54 +0900

    [fix] 코드 수정 #19

--- src/views/wish/ReviewDetail.js
+++ src/views/wish/ReviewDetail.js
@@ -6,6 +6,33 @@ import star from "../../resources/img/Movie/star.png";
 import starN from "../../resources/img/Movie/star_unclick.png";
 
 const ReviewDetail=() =>{
+    const reviews = {
+        '영화명1': [
+            {
+                criticName: '이은선',
+                stars: 3,
+                reviewTitle: '리뷰 소제목',
+                reviewContent: '리뷰리뷰리뷰리뷰리뷰~',
+            },
+            {
+                criticName: '박예린',
+                stars: 2,
+                reviewTitle: '리뷰 소제목',
+                reviewContent: '리뷰리뷰리뷰리뷰리뷰~',
+            },
+            // 추가적인 리뷰 데이터들...
+        ],
+        '영화명2': [
+            {
+                criticName: '이은선',
+                stars: 5,
+                reviewTitle: '리뷰 소제목',
+                reviewContent: '리뷰리뷰리뷰리뷰리뷰~',
+            },
+        ],
+        // 추가적인 영화 리뷰들...
+    };
+
     return (
         <div className="mobile">
             <div className="back_img">
@@ -13,90 +40,41 @@ const ReviewDetail=() =>{
                 좋아하는 리뷰
             </div>
 
-            <div className="critic_title">
-                영화명
-            </div>
-
-            <div className="critic">
-                <div className="critic_name">
-                    이은선
-                    <div className="star_critic">
-                        <img className="star_image" src={star} width={25} height={25}/>
-                        <img className="star_image" src={star} width={25} height={25}/>
-                        <img className="star_image" src={star} width={25} height={25}/>
-                        <img className="star_image" src={starN} width={25} height={25}/>
-                        <img className="star_image" src={starN} width={25} height={25}/>
+            {/* 각 영화별 리뷰를 출력 */}
+            {Object.keys(reviews).map(movieTitle => (
+                <div key={movieTitle}>
+                    <div className="critic_title">
+                        {movieTitle}
                     </div>
-                    <div className="bookmark_critic">
-                        <img src={bookmark} width={35} height={35}/>
-                    </div>
+
+                    {/* 특정 영화명에 해당하는 리뷰들만 출력 */}
+                    {reviews[movieTitle].map((review, idx) => (
+                        <div key={idx} className="critic">
+                            <div className="critic_name">
+                                {review.criticName}
+                                <div className="star_critic">
+                                    {Array.from({ length: review.stars }).map((_, i) => (
+                                        <img key={i} className="star_image" src={star} width={25} height={25}/>
+                                    ))}
+                                    {Array.from({ length: 5 - review.stars }).map((_, i) => (
+                                        <img key={review.stars + i} className="star_image" src={starN} width={25} height={25}/>
+                                    ))}
+                                </div>
+                                <div className="bookmark_critic">
+                                    <img src={bookmark} width={35} height={35}/>
+                                </div>
+                            </div>
+                            <div className="critic_review_mid">
+                                {review.reviewTitle}
+                            </div>
+                            <div className="critic_review">
+                                {review.reviewContent}
+                            </div>
+                            <div className="horizontal-line"></div>
+                        </div>
+                    ))}
                 </div>
-
-
-                <div className="critic_review_mid">
-                    리뷰 소제목
-                </div>
-                <div className="critic_review">
-                    리뷰리뷰리뷰리뷰리뷰~
-                </div>
-
-                <div className="horizontal-line"></div>
-            </div>
-            <div className="critic">
-                <div className="critic_name">
-                    이은선
-                    <div className="star_critic">
-                        <img className="star_image" src={star} width={25} height={25}/>
-                        <img className="star_image" src={star} width={25} height={25}/>
-                        <img className="star_image" src={star} width={25} height={25}/>
-                        <img className="star_image" src={starN} width={25} height={25}/>
-                        <img className="star_image" src={starN} width={25} height={25}/>
-                    </div>
-                    <div className="bookmark_critic">
-                        <img src={bookmark} width={35} height={35}/>
-                    </div>
-                </div>
-
-
-                <div className="critic_review_mid">
-                    리뷰 소제목
-                </div>
-                <div className="critic_review">
-                    리뷰리뷰리뷰리뷰리뷰~
-                </div>
-
-                <div className="horizontal-line"></div>
-            </div>
-
-            <div className="critic_title">
-                영화명
-            </div>
-
-            <div className="critic">
-                <div className="critic_name">
-                    이은선
-                    <div className="star_critic">
-                        <img className="star_image" src={star} width={25} height={25}/>
-                        <img className="star_image" src={star} width={25} height={25}/>
-                        <img className="star_image" src={star} width={25} height={25}/>
-                        <img className="star_image" src={starN} width={25} height={25}/>
-                        <img className="star_image" src={starN} width={25} height={25}/>
-                    </div>
-                    <div className="bookmark_critic">
-                        <img src={bookmark} width={35} height={35}/>
-                    </div>
-                </div>
-
-
-                <div className="critic_review_mid">
-                    리뷰 소제목
-                </div>
-                <div className="critic_review">
-                    리뷰리뷰리뷰리뷰리뷰~
-                </div>
-
-                <div className="horizontal-line"></div>
-            </div>
+            ))}
 
         </div>
     );
