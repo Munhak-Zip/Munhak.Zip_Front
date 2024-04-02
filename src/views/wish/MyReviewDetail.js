30b26a3ed9227d7ea31b907ee6bf107bf181c32c
Dayun Lee <wohd7877@naver.com>
2024-04-02 12:00:38 +0900

    [fix] 코드 수정 #19

--- src/views/wish/MyReviewDetail.js
+++ src/views/wish/MyReviewDetail.js
@@ -5,57 +5,60 @@ import React from "react";
 import '../../resources/css/Wish/ReviewDetail.css';
 
 const MyReviewDetail=() =>{
+    const reviews = {
+        '영화명1': [
+            {
+                stars: 3,
+                reviewTitle: '리뷰 소제목',
+                reviewContent: '리뷰리뷰리뷰리뷰리뷰~',
+            }
+        ],
+        '영화명2': [
+            {
+                stars: 5,
+                reviewTitle: '리뷰 소제목',
+                reviewContent: '리뷰리뷰리뷰리뷰리뷰~',
+            }
+        ],
+        // 추가적인 영화 리뷰들...
+    };
+
     return (
+
         <div className="mobile">
             <div className="back_img">
                 <img src={back} width={30} height={30}/>
                 <span>나의 리뷰</span>
             </div>
+            {Object.keys(reviews).map(movieTitle => (
+                <div key={movieTitle}>
+                    <div className="critic_title">
+                        {movieTitle}
+                    </div>
 
-            <div className="critic_title">
-                영화명
-            </div>
+                    {/* 특정 영화명에 해당하는 리뷰들만 출력 */}
+                    {reviews[movieTitle].map((review, idx) => (
+                        <div key={idx} className="critic">
+                            <div className="star_critic">
+                                {Array.from({ length: review.stars }).map((_, i) => (
+                                    <img key={i} className="star_image" src={star} width={25} height={25}/>
+                                ))}
+                                {Array.from({ length: 5 - review.stars }).map((_, i) => (
+                                    <img key={review.stars + i} className="star_image" src={starN} width={25} height={25}/>
+                                ))}
+                            </div>
 
-            <div className="critic">
-                <div className="star_critic">
-                    <img className="star_image" src={star} width={25} height={25}/>
-                    <img className="star_image" src={star} width={25} height={25}/>
-                    <img className="star_image" src={star} width={25} height={25}/>
-                    <img className="star_image" src={starN} width={25} height={25}/>
-                    <img className="star_image" src={starN} width={25} height={25}/>
+                            <div className="critic_review_mid">
+                                {review.reviewTitle}
+                            </div>
+                            <div className="critic_review">
+                                {review.reviewContent}
+                            </div>
+                            <div className="horizontal-line"></div>
+                        </div>
+                    ))}
                 </div>
-
-                <div className="critic_review_mid">
-                    리뷰 소제목
-                </div>
-                <div className="critic_review">
-                    리뷰리뷰리뷰리뷰리뷰~
-                </div>
-                <div className="horizontal-line"></div>
-            </div>
-
-
-            <div className="critic_title">
-                영화명
-            </div>
-
-            <div className="critic">
-                <div className="star_critic">
-                    <img className="star_image" src={star} width={25} height={25}/>
-                    <img className="star_image" src={star} width={25} height={25}/>
-                    <img className="star_image" src={star} width={25} height={25}/>
-                    <img className="star_image" src={starN} width={25} height={25}/>
-                    <img className="star_image" src={starN} width={25} height={25}/>
-                </div>
-
-                <div className="critic_review_mid">
-                    리뷰 소제목
-                </div>
-                <div className="critic_review">
-                    리뷰리뷰리뷰리뷰리뷰~
-                </div>
-                <div className="horizontal-line"></div>
-            </div>
+            ))}
 
         </div>
     );
