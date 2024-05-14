9dc4a35650d92ee9d8e197e3f9126b41f6bfbd87
yjin-jo <dpwls5100@icloud.com>
2024-05-14 10:40:49 +0900

    [add] : 영화 데이터 배열 #23

--- src/views/movie/MovieList.js
+++ src/views/movie/MovieList.js
@@ -4,6 +4,9 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {useState} from 'react';
 import '../../resources/css/Movie/MovieList.css'
 import Arrow from '../../resources/next.png'
+import Poster from '../../resources/img/Main/sample1.png'
+import Star from '../../resources/img/Movie/star.png'
+
 function Header(props) {
     console.log('props', props, props.title)
     return <header>
@@ -13,6 +16,27 @@ function Header(props) {
         }}>{props.title}</a></h1>
     </header>
 }
+const renderMovies = () => {
+    // 영화 데이터 배열 (실제 영화 데이터로 교체 가능)
+    const movies = [
+        { id: 1, title: '영화명', poster: Poster, start: 5 },
+        { id: 2, title: '영화명', poster: Poster, start: 5 },
+
+        // 필요한 만큼 영화 객체 추가
+    ];
+
+    // 영화 배열을 순회하며 각 영화 포스터를 렌더링합니다.
+    return movies.map(movie => (
+        <span key={movie.id} className="movie">
+                <img src={movie.poster} alt={movie.title} className="Poster-img"/>
+                <p>{movie.title}<img src={Star} className={"star"}/>({movie.start})</p>
+            </span>
+    ));
+
+}
+const showMovies = () => {
+
+}
 function Category(props) {
     let content;
     if (props.type === "wishMovies") {
@@ -21,11 +45,10 @@ function Category(props) {
     return <div className={props.type}>
         <img src={Arrow} className={"Arrow"}/>
         {content}
+        <div className={"movies"}>
+            {renderMovies()}
+        </div>
     </div>
-}
-
-function Article(props) {
-
 }
 
 function App() {
