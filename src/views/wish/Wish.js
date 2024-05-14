b0f9f0061336dd2a6aa641428193bd4ac5578a96
yjin-jo <dpwls5100@icloud.com>
2024-05-14 16:39:33 +0900

    [add] : renderMovies 추가 #25

--- src/views/wish/Wish.js
+++ src/views/wish/Wish.js
@@ -13,6 +13,26 @@ const Wish=() => {
             }}>{props.title}</a></h1>
         </header>
     }
+    const renderMovies = () => {
+        // 영화 데이터 배열 (실제 영화 데이터로 교체 가능)
+        const movies = [
+            { id: 1, title: '영화명', poster: Poster, start: 5 },
+            { id: 2, title: '영화명', poster: Poster, start: 5 },
+            { id: 3, title: '영화명', poster: Poster, start: 5 },
+            { id: 4, title: '영화명', poster: Poster, start: 5 },
+
+            // 필요한 만큼 영화 객체 추가
+        ];
+
+        // 영화 배열을 순회하며 각 영화 포스터를 렌더링합니다.
+        return movies.map(movie => (
+            <span key={movie.id} className="movie">
+                <img src={movie.poster} alt={movie.title} className="Poster-img"/>
+                <p>{movie.title}<img src={Star} className={"star"}/>({movie.start})</p>
+            </span>
+        ));
+
+    }
     function Category(props) {
         let content;
         if (props.type === "wishMovies") {
