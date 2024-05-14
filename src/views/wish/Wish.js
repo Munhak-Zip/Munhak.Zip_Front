41c64fe0c83d23763be83911216c62ec8606e2f7
yjin-jo <dpwls5100@icloud.com>
2024-05-14 16:37:55 +0900

    [add] : Wish.js 추가 #25

--- src/views/wish/Wish.js
+++ src/views/wish/Wish.js
@@ -1,7 +1,23 @@
+import React, {useState} from "react";
 
 const Wish=() => {
-    return (
+    const [mode, setMode] = useState('WELCOME');
+    function Header(props) {
+        console.log('props', props, props.title)
+        return <header>
+            <h1><a href="/" onClick={(event)=> {
+                event.preventDefault();
+                props.onChangeMode();
+            }}>{props.title}</a></h1>
+        </header>
+    }
 
+    return (
+        <div className={"div1"}>
+            <Header title={"MOVIE.ZIP"} onChangeMode={() => {
+                setMode('WELCOME');
+            }}></Header>
+        </div>
     )
 }
 export default Wish;
