ad88c8a65eb21fc254ea21e83827d71df5a81196
yjin-jo <dpwls5100@icloud.com>
2024-05-14 10:02:34 +0900

    [add] : Category 태그 추가 #23

--- src/views/movie/MovieList.js
+++ src/views/movie/MovieList.js
@@ -5,10 +5,15 @@ import {useState} from 'react';
 import '../../resources/css/Movie/MovieList.css'
 
 function Header(props) {
-
+    console.log('props', props, props.title)
+    return <header>
+        <h1><a href="/" onClick={(event)=> {
+            event.preventDefault();
+            props.onChangeMode();
+        }}>{props.title}</a></h1>
+    </header>
 }
-
-function Nav(props) {
+function Category(props) {
 
 }
 
@@ -23,37 +28,12 @@ function App() {
 
     const [mode, setMode] = useState('WELCOME');
     const [search, setSearch] = useState(""); //검색변수
-    const onChange = (event) => {
-        setSearch(event.target.value)
-    }
+
     const [moviesIndex, setMoviesIndex] = useState(0); // 현재 영화 리스트의 시작 인덱스
 
 
-    const [id, setId] = useState(null);
-    const topics = [
-        {id: 1, title: '보관함', body: 'wish is...'},
-        {id: 2, title: '마이페이지', body: 'myPage...'},
-        {id:3, title:'로그아웃', body:'logOut...'}
-    ]
-    let content = null;
-    if(mode === 'WELCOME'){
-        content = <Article title="Welcome" body={"Hello, MOVIE.ZIP"}></Article>
-    } else if(mode === 'READ'){
-        let title,body = null
-        for(let i=0; i<topics.length; i++){
-            console.log(topics[i].id, id);
-            if(topics[i].id === id) {
-                title=topics[i].title;
-                body=topics[i].body;
-            }
-        }
-        content = <Article title={title} body={body}></Article>
-    }
 
     const renderMovies = () => {
-
-    }
-    const showMovies = () => {
 
     }
 
@@ -67,17 +47,9 @@ function App() {
             <Header title={"MOVIE.ZIP"} onChangeMode={() => {
                 setMode('WELCOME');
             }}></Header>
-            <input type="text" placeholder={"검색하기"} value={search}/>
-            <input type={"button"} value={"검색"}/>
-            {/*<Nav topics={topics} onChangeMode={(_id)=>{
-                setMode('READ');
-                setId(_id);
-            }}></Nav>
-            {content}*/}
-            <p/>
-            <Movies type={"new"}></Movies>
-            <Movies type={"recommend"}></Movies>
-            <Movies type={"wish"}></Movies>
+            <Category>
+
+            </Category>
         </div>
     )
 }
