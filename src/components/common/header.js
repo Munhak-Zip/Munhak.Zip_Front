b7ccc70a0260cd725ef166b2069ccde5d0544756
Dayun Lee <wohd7877@naver.com>
2024-05-17 11:13:45 +0900

    [feat/#28] 헤더 이미지 수정

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Link, useNavigate} from "react-router-dom";
 import menu from '../../resources/img/menu.png';
+import '../../resources/css/Common/header.css';
 
 const header=() =>{
     return (
         <header>
-            <div>
-                <img src={menu} width={250} height={250}/>
+            <div className= "mobile">
+                <img src={menu} width={50} height={50}/>
             </div>
         </header>
     )
