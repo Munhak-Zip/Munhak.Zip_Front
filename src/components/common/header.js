7a98ce951b25c2225eb548177195a0c9cf19f2d0
Dayun Lee <wohd7877@naver.com>
2024-05-17 12:48:04 +0900

    [feat/#28] 헤더 부분에만 들어가게 추가

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import {Link, useNavigate} from "react-router-dom";
+import {Link, useLocation, useNavigate} from "react-router-dom";
 import menu from '../../resources/img/menu.png';
 import '../../resources/css/Common/header.css';
 
 const header=() =>{
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const locationNow = useLocation();
+
+    //안보여주고 싶은 화면
+    if (locationNow.pathname === "/main") return null;
     return (
         <header className="header">
             <div className= "mobile">
