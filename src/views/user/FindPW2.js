7837f0a3b001d34a3fef838c38810b82cf1197c4
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2024-06-21 02:43:11 +0900

    add: 비밀번호 찾기 #52

--- src/views/user/FindPW2.js
+++ src/views/user/FindPW2.js
@@ -1,20 +1,43 @@
 import React, { useState } from 'react';
 import '../../resources/css/User/FindPW2.css';
+import axios from 'axios';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const FindPW2 = () => {
     const [pw, setPw] = useState("");
+    const location = useLocation();
+    const { id } = location.state || {};
+    const navigate = useNavigate();
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
         console.log("변경할 비밀번호:", pw);
+        try {
+            const response = await axios.post('/changePassword', {
+                userId: id,
+                newPassword: pw
+            });
+
+            alert(`비밀번호 변경 성공`);
+            navigate('/login');
+        } catch (error) {
+            console.log("비밀번호 변경 실패");
+            alert('비밀번호 변경 실패');
+        }
     };
 
     return (
         <div className="findPW2">
+            {id ? (
+                <p>User ID: {id}</p>
+            ) : (
+                <p>No User ID found</p>
+            )}
+
             <form onSubmit={handleSubmit}>
                 <input
                     type="password"
-                    placeholder="PW"
+                    placeholder="새로운 비밀번호를 입력하세요"
                     value={pw}
                     onChange={(e) => setPw(e.target.value)}
                 />
